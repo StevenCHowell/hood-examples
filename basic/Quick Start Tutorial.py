975bebab7b3be00ba77c118a464766168293d3d9
Steven C. Howell <steven.howell@nist.gov>
2016-11-07 09:28:34 -0700

    add plot of starting configuration

--- basic/Quick Start Tutorial.py
+++ basic/Quick Start Tutorial.py
@@ -20,3 +20,18 @@ md.integrate.brownian(group=all, kT=0.1, seed=987)
 
 # run 200,000 time steps
 run(2e5)
+
+# take a snapshot of the current state of the system and save the particle positions
+snapshot = system.take_snapshot(all=True)
+pos = snapshot.particles.position
+
+# a visual representation using matplotlib
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for part in pos:
+    ax.scatter(part[0], part[1], part[2], c='y')
+plt.show()
